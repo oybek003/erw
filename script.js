838a442056d9a8fc911ee1c0adaf894d77682d29
crazy_simple <ojbekmuslimov1@gmail.com>
2025-01-18 15:15:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function lightcolor() {
+  alert("LightMode Funksiyasi ishldi");
+  document.body.style.backgroundColor = "#C4B5A5";
+  document.body.style.color = "black";
+}
+
+function darkcolor() {
+  alert("DarkMode Funksiyasi ishldi");
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
